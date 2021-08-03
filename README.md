3ef42d43c926886068628dcef29c1417c12a816a
Hyeseong-int <a01025819869@gmail.com>
2021-08-03 16:37:47 +0900

    실습환경 설정

---
+++ README.md
@@ -0,0 +1,18 @@
+# web4-session
+생활코딩 Web-4 session를 학습하는 레포
+
+
+Node.js 웹 프레임워크인 Express에서 Session을 이용해 인증(login)을 구현하는 방법을 학습.
+
+###### 학습 링크 https://opentutorials.org/course/3400/21831
+
+
+###### 학습 내용
+<p>
+    <img src="https://img.shields.io/badge/Node.js-339933?style=flat-square&logo=Node.js&logoColor=white"/>&nbsp
+    <img src=https://img.shields.io/badge/Express-000000?style=flat-square&logo=Express&logoColor=white"/>&nbsp
+    <img src="https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white"/>&nbsp 
+</p>
+
+##### Session(express-session): https://github.com/expressjs/session#readme
+
---
+++ routes/auth.js
@@ -0,0 +1,57 @@
+var express = require('express')
+var router = express.Router()
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
+var template = require('../lib/template.js');
+var fs = require('fs');
+
+var authData = {
+  email:'test@test.com',
+  password:'1111',
+  nickname:'hyeseong'
+}
+
+router.get('/login', function(request, response){
+  var title = 'WEB - login';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list, `
+    <form action="/auth/login_process" method="post">
+      <p><input type="text" name="email" placeholder="email"></p>
+      <p>
+      <input type="password" name="pwd" placeholder="password">
+      </p>
+      <p>
+        <input type="submit" value="login">
+      </p>
+    </form>
+  `, '');
+  response.send(html);
+});
+
+router.post('/login_process', function(request, response){
+  var post = request.body;
+  var email = post.email;
+  var password = post.pwd;
+  if(email === authData.email && password === authData.password){
+    //success!
+    request.session.is_logined = true;
+    request.session.nickname = authData.nickname;
+    /*
+    바로 리다이렉트를 시키니까 아무리 해도 로그인 불가. 리다이렉트 대신 send()를 사용하니 로그인 성공.
+    바로 리다이렉트를 시킬 경우, 세션 데이터를 저장하기도 전에 리다이렉트를 시켜버려서 로그인 불가능.
+    */
+    request.session.save(function(){
+      response.redirect(`/`);
+    })
+  }else{
+    response.send('Who?');
+  }
+});
+
+router.get('/logout', function(request, response){
+  request.session.destroy(function(err){
+    response.redirect('/');
+  });
+});
+
+module.exports = router;
