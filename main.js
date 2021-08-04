3e65f7d968f6706db1baba58c532d6663b4d6b4e
Hyeseong-int <a01025819869@gmail.com>
2021-08-04 14:19:07 +0900

    web5-passport_impl session in passport

--- main.js
+++ main.js
@@ -31,6 +31,20 @@ var authData = {
 var passport = require('passport')
  , LocalStrategy = require('passport-local').Strategy;
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  console.log('seriallizeUser', user);
+  done(null, user.email);
+  // done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  console.log('deserializeUser', id);
+  done(null, authData);
+});
+
 passport.use(new LocalStrategy(
   {
     usernameField: 'email',
@@ -39,18 +53,14 @@ passport.use(new LocalStrategy(
  function(username, password, done) {
    console.log('LocalStrategy', username, password);
    if(username === authData.email){
-     console.log(1);
      if(password === authData.password){
-      console.log(2); 
-      return done(null, user);
+      return done(null, authData);
      }else {
-       console.log(3);
        return done(null, false, {
          message: 'Incorrect password.'
        })
      }
    }else {
-     console.log(4);
      return done(null, false, {
        message: 'Incorrect username.'
      })
